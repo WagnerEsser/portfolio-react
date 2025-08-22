af04366dadb0b779e4b1fb61362724fc7ecc528a
Wagner Esser <waesser@gmail.com>
2025-08-22 00:46:39 -0300

    feat: adjusts path aliases on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,20 +7,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src'),
-      },
-    ],
-    // {
-    //   '@assets': path.resolve(__dirname, 'src/assets'),
-    //   '@components': path.resolve(__dirname, 'src/components'),
-    //   '@hooks': path.resolve(__dirname, 'src/hooks'),
-    //   '@pages': path.resolve(__dirname, 'src/pages'),
-    //   '@servicess': path.resolve(__dirname, 'src/servicess'),
-    //   '@stores': path.resolve(__dirname, 'src/stores'),
-    //   '@types': path.resolve(__dirname, 'src/types'),
-    // },
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@stores': path.resolve(__dirname, 'src/stores'),
+      '@types': path.resolve(__dirname, 'src/types'),
+    },
   },
 });
